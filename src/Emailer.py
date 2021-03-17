4e3ad523378b20828e815df839774195ce877a52
farinaanthony96 <farinaanthony96@gmail.com>
2021-03-17 15:40:54 -0400

    v1.0 - Can send emails w/ attachments

---
+++ src/Emailer.py
@@ -0,0 +1,102 @@
+import configparser
+import smtplib
+import email.utils
+
+from configparser import ExtendedInterpolation
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+# Module information.
+__author__ = 'Anthony Farina'
+__copyright__ = 'Copyright 2021, Emailer'
+__credits__ = ['Anthony Farina']
+__license__ = 'MIT'
+__version__ = '1.0.0'
+__maintainer__ = 'Anthony Farina'
+__email__ = 'farinaanthony96@gmail.com'
+__status__ = 'Released'
+
+
+# Prepare the config file reference.
+CONFIG = configparser.ConfigParser(interpolation=ExtendedInterpolation())
+CONFIG.read('../config.ini')
+
+# Prepare SMTP-related variables from the config file.
+SMTP_SERVER = CONFIG['SMTP Info']['server']
+SMTP_PORT = CONFIG['SMTP Info']['port']
+SMTP_USERNAME = CONFIG['SMTP Info']['username']
+SMTP_PASSWORD = CONFIG['SMTP Info']['password']
+
+# Prepare party-related variables from the config file.
+SENDER = CONFIG['Parties']['sender']
+SENDER_NAME = CONFIG['Parties']['sender_name']
+RECIPIENT = CONFIG['Parties']['recipient']
+
+# Prepare message content variables from the config file.
+SUBJECT = CONFIG['Message Contents']['subject']
+MSG_BODY = CONFIG['Message Contents']['msg_body'].replace('\\\\n', '\n')
+ATTACHMENTS = CONFIG['Message Contents']['attachments'].split(',')
+
+
+# Sends an email from the sender email address to the recipient email
+# address using the SMTP server specified in the config file. The email
+# contains a subject, message body, and attachments also specified in the
+# config file.
+def emailer() -> None:
+    # Prepare the MIME object for the email.
+    message = MIMEMultipart()
+    message['Subject'] = SUBJECT
+    message['From'] = email.utils.formataddr((SENDER_NAME, SENDER))
+    message['To'] = RECIPIENT
+
+    # Attach the message body to the email.
+    msg_attachment = MIMEText(MSG_BODY, 'plain')
+    message.attach(msg_attachment)
+
+    # Attach all the files referenced in the config file to the email.
+    for filename in ATTACHMENTS:
+        # Prepare the file attachment by reading the file as bytes.
+        file_attachment = MIMEBase('application', 'octet-stream')
+        file_attachment.set_payload(open(filename, 'rb').read())
+
+        # Encode the file attachment in ASCII.
+        encoders.encode_base64(file_attachment)
+
+        # Add the header as a key/value pair to the file attachment.
+        file_attachment.add_header('Content-Disposition', 'attachment',
+                                   filename=filename)
+
+        # Add the file attachment to the email.
+        message.attach(file_attachment)
+
+    # Try to send the email.
+    try:
+        # Create SMTP server connection.
+        server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+
+        # Encrypt the connection via TLS.
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+
+        # Login and send the email.
+        server.login(SMTP_USERNAME, SMTP_PASSWORD)
+        server.sendmail(SENDER, RECIPIENT, message.as_string())
+
+        # Close the connection to the SMTP server.
+        server.close()
+    # Print the error to the console in case something goes wrong.
+    except Exception as e:
+        print("Error: ", e)
+    # Print a success message to the console.
+    else:
+        print("Email sent!")
+
+
+# Main method that runs the emailer script.
+if __name__ == '__main__':
+    # Run the script.
+    emailer()
